0ea3032b4a152c05555873c57644f9ba1e35517b
liujiandong <liujiandong@iprexmed.com>
2017-11-06 17:12:58 +0800

    add

--- crawl.go
+++ crawl.go
@@ -6,7 +6,6 @@ import (
 	"log"
 	"os"
 	"os/signal"
-	"time"
 
 	"github.com/PuerkitoBio/goquery"
 )
@@ -24,8 +23,8 @@ type Worker interface {
 
 func init() {
 	var config Config
-	flag.IntVar(&config.NumWorker, "n", 10, "usage: -n [NumWorker].\n")
-	flag.IntVar(&config.LenQueue, "l", 100, "usage: -l [LenQueue].\n")
+	flag.IntVar(&config.NumWorker, "n", 1, "usage: -n [NumWorker].\n")
+	flag.IntVar(&config.LenQueue, "l", 1, "usage: -l [LenQueue].\n")
 	flag.Parse()
 
 	c = make(chan Worker, config.LenQueue)
@@ -34,6 +33,17 @@ func init() {
 		go run(i)
 		fmt.Printf("init %d complete.\n", i)
 	}
+}
+
+func main() {
+	_ = goquery.Document{}
+
+	testCase := []Model{
+		{Name: "taobao", URL: "http://www.taobao.com", Labels: []Label{Label{Route: "div #J_SiteNav a", Attrs: []string{"href"}}}},
+	}
+	for _, m := range testCase {
+		c <- m
+	}
 
 	var signalChan = make(chan os.Signal)
 	signal.Notify(signalChan, os.Interrupt)
@@ -41,33 +51,40 @@ func init() {
 	<-signalChan
 }
 
-func main() {
-	_ = goquery.Document{}
-
-	testCase := []Model{
-		{Name: "jd"},
-		{Name: "je"},
-		{Name: "jf"},
-		{Name: "jg"},
-		{Name: "jh"},
-	}
-	for i := 0; i < 1000; i++ {
-		for _, m := range testCase {
-			c <- m
-		}
-	}
-	time.Sleep(time.Second * 2)
-}
-
 type Model struct {
 	Name   string
-	URL    string   // 链接
-	Labels []string // 标签路径
-	Attrs  []string // 属性
+	URL    string // 链接
+	Labels []Label
+}
+
+type Label struct {
+	Route string   // 标签路径
+	Attrs []string // 属性
+	Loop  bool     // 遍历
 }
 
 func (m Model) Work(id int) error {
-	fmt.Printf("%d: %s\n", id, m.Name)
+	doc, err := goquery.NewDocument(m.URL)
+	if err != nil {
+		return err
+	}
+
+	for _, label := range m.Labels {
+		sel := doc.Find(label.Route)
+		for _, attr := range label.Attrs {
+			attrValue, ok := sel.Attr(attr)
+			if !ok {
+				htmlContent, err := sel.Html()
+				if err != nil {
+					return err
+				}
+				log.Printf("count find %s, %s, %s\n", label.Route, attr, htmlContent)
+				continue
+			}
+			fmt.Println(attrValue)
+		}
+	}
+
 	return nil
 }
 
@@ -75,7 +92,7 @@ func run(i int) {
 	for w := range c {
 		err := w.Work(i)
 		if err != nil {
-			log.Fatal(i, err)
+			log.Println(i, err)
 		}
 	}
 }
